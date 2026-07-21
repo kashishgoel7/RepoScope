232c713eff47938fd0ce44045cea6f57124cb15f
Kashish Goel <kashishgoel74@gmail.com>
2026-07-22 00:50:47 +0530

    controller changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,9 +15,9 @@ const generateToken = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    secure: true,
+    sameSite: "none",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
   return token;
